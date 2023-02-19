ebe6fb5ab4045c78b510a93bfc4c6b4c790af470
nbabcock <nathan.r.babcock@gmail.com>
2023-02-19 15:18:08 -0600

    fix: update incorrect args in testsrc rustdoc

--- src/command.rs
+++ src/command.rs
@@ -461,8 +461,8 @@ impl FfmpegCommand {
 
   //// Preset argument sets for common use cases.
 
-  /// Generate a procedural test video. Equivalent to `ffmpeg -i lavfi -f
-  /// testsrc`
+  /// Generate a procedural test video. Equivalent to `ffmpeg -f lavfi -i
+  /// testsrc=duration=10`.
   ///
   /// [FFmpeg `testsrc` filter
   /// documentation](https://ffmpeg.org/ffmpeg-filters.html#allrgb_002c-allyuv_002c-color_002c-colorchart_002c-colorspectrum_002c-haldclutsrc_002c-nullsrc_002c-pal75bars_002c-pal100bars_002c-rgbtestsrc_002c-smptebars_002c-smptehdbars_002c-testsrc_002c-testsrc2_002c-yuvtestsrc)
