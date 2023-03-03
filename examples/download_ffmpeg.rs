5e707e575bd0a34d934e81c8fd162684e6a0c172
nbabcock <nathan.r.babcock@gmail.com>
2023-03-03 17:38:33 -0600

    feat: extract ffmpeg binaries on windows

--- examples/download_ffmpeg.rs
+++ examples/download_ffmpeg.rs
@@ -1,9 +1,15 @@
-use ffmpeg_sidecar::auto_download::{check_latest_version, download_ffmpeg};
+use std::fs::rename;
+
+use ffmpeg_sidecar::auto_download::{check_latest_version, download_ffmpeg, unpack_ffmpeg};
 
 fn main() {
-  let latest_version = check_latest_version().unwrap();
-  println!("{}", latest_version);
+  // let latest_version = check_latest_version().unwrap();
+  // println!("{}", latest_version);
 
-  let filename = download_ffmpeg().unwrap();
-  println!("{}", filename);
+  // let filename = download_ffmpeg().unwrap();
+  // println!("{}", filename);
+
+  unpack_ffmpeg("ffmpeg-release-essentials.zip").unwrap();
+
+  // rename("temp/bop.txt", "bop.txt").unwrap();
 }
