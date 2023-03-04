308622529addea57a3e980031351828c769f4cc7
nbabcock <nathan.r.babcock@gmail.com>
2023-03-03 18:35:21 -0600

    refactor: extract binaries adjacent to rust binary

--- examples/download_ffmpeg.rs
+++ examples/download_ffmpeg.rs
@@ -1,8 +1,28 @@
-use std::fs::rename;
-
-use ffmpeg_sidecar::auto_download::{check_latest_version, download_ffmpeg, unpack_ffmpeg};
+use ffmpeg_sidecar::download::{
+  check_latest_version, download_ffmpeg_package, ffmpeg_is_installed, unpack_ffmpeg,
+};
 
 fn main() {
-  let filename = download_ffmpeg().unwrap();
+  if ffmpeg_is_installed() {
+    println!("FFmpeg is already installed! 🎉");
+    println!("For demo purposes, we'll re-download and unpack it anyway.");
+  }
+
+  // TODO: check if the 3 binary files are already present
+  // TODO: if so, prompt to delete them
+
+  let latest_version = check_latest_version().unwrap();
+  println!("Latest available version: {}", latest_version);
+
+  let filename = download_ffmpeg_package().unwrap();
+  println!("Downloaded package: {}", filename);
+
   unpack_ffmpeg(&filename).unwrap();
+  println!("Done! ✨");
+
+  // TODO: verify that the 3 binary files are present
+
+  if !ffmpeg_is_installed() {
+    panic!()
+  }
 }
