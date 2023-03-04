c40c7eac549d73f38436ab9894b42d1f6b7c43e9
nbabcock <nathan.r.babcock@gmail.com>
2023-03-03 19:33:29 -0600

    fix: download all temporary files to rust binary folder

--- src/download.rs
+++ src/download.rs
@@ -2,7 +2,7 @@ use std::{
   env::{consts::OS, current_exe},
   fs::{create_dir_all, read_dir, remove_dir_all, remove_file, rename},
   io::Read,
-  path::Path,
+  path::{Path, PathBuf},
   process::{Command, ExitStatus, Stdio},
 };
 
@@ -114,37 +114,42 @@ pub fn get_download_url() -> Option<&'static str> {
   }
 }
 
+/// By default, downloads all temporary files to the same directory as the Rust executable.
+pub fn get_download_dir() -> Result<PathBuf> {
+  current_exe()?
+    .parent()
+    .ok_or_else(|| Error::from(()))
+    .map(|p| p.to_owned())
+}
+
 /// Downloads an archive (ZIP on windows, TAR on linux and mac)
 /// from the latest published release online.
-pub fn download_ffmpeg_package() -> Result<String> {
+pub fn download_ffmpeg_package() -> Result<PathBuf> {
   let url = get_download_url().ok_or(Error::msg("Unsupported platform"))?;
 
   let filename = Path::new(url)
     .file_name()
     .ok_or(Error::msg("Failed to get filename"))?;
 
-  let exit_status = curl_to_file(
-    url,
-    filename
-      .to_str()
-      .ok_or(Error::msg("Failed to get filename"))?,
-  )?;
+  let archive_path = get_download_dir()?.join(filename);
+
+  let exit_status = curl_to_file(url, archive_path.to_str().ok_or("invalid download path")?)?;
 
   if !exit_status.success() {
     return Err(Error::msg("Failed to download ffmpeg"));
   }
 
-  Ok(filename.to_string_lossy().to_string())
+  Ok(archive_path)
 }
 
 /// After downloading, unpacks the archive, moves the binaries, and cleans up.
-pub fn unpack_ffmpeg(filename: &str) -> Result<()> {
+pub fn unpack_ffmpeg(archive_path: &PathBuf) -> Result<()> {
   create_dir_all(UNPACK_DIR)?;
 
   // Extract archive
   Command::new("tar")
     .arg("-xf")
-    .arg(filename)
+    .arg(archive_path.to_str().ok_or("invalid archive path")?)
     .args(["-C", UNPACK_DIR])
     .status()?
     .success()
@@ -152,7 +157,7 @@ pub fn unpack_ffmpeg(filename: &str) -> Result<()> {
     .ok_or("Failed to unpack ffmpeg")?;
 
   // Move binaries
-  let cwd = current_exe()?.parent().ok_or(())?.to_owned();
+  let download_dir = get_download_dir()?;
   let (ffmpeg, ffplay, ffprobe) = match OS {
     "windows" => {
       let inner_folder = read_dir(UNPACK_DIR)?
@@ -176,12 +181,14 @@ pub fn unpack_ffmpeg(filename: &str) -> Result<()> {
     _ => return Err(Error::msg("Unsupported platform")),
   };
 
-  rename(&ffmpeg, cwd.join(ffmpeg.file_name().ok_or(())?))?;
-  rename(&ffplay, cwd.join(ffplay.file_name().ok_or(())?))?;
-  rename(&ffprobe, cwd.join(ffprobe.file_name().ok_or(())?))?;
+  // Move binaries
+  rename(&ffmpeg, download_dir.join(ffmpeg.file_name().ok_or(())?))?;
+  rename(&ffplay, download_dir.join(ffplay.file_name().ok_or(())?))?;
+  rename(&ffprobe, download_dir.join(ffprobe.file_name().ok_or(())?))?;
 
+  // Delete archive and unpacked files
   remove_dir_all(UNPACK_DIR)?;
-  remove_file(filename)?;
+  remove_file(archive_path)?;
 
   Ok(())
 }
