845d8a7b0242da1262b49a2b3f0e82377d098e37
nbabcock <nathan.r.babcock@gmail.com>
2024-10-30 17:12:27 -0500

    feat: add subtitle track to named pipe demo

--- examples/named_pipes.rs
+++ examples/named_pipes.rs
@@ -10,33 +10,41 @@ use std::thread;
 fn main() -> Result<()> {
   const VIDEO_PIPE_NAME: &'static str = r#"\\.\pipe\ffmpeg_video"#;
   const AUDIO_PIPE_NAME: &'static str = r#"\\.\pipe\ffmpeg_audio"#;
+  const SUBTITLES_PIPE_NAME: &'static str = r#"\\.\pipe\ffmpeg_subtitles"#;
 
-  // Prepare an FFmpeg command with separate outputs for video and audio
+  // Prepare an FFmpeg command with separate outputs for video, audio, and subtitles
   let mut command = FfmpegCommand::new();
   command
-    // Global flags:
+    // Global flags
     .hide_banner()
     .overwrite() // <- overwrite reqired on windows
-    // Generate test video:
+    // Generate test video
     .format("lavfi")
     .input(format!("testsrc=size=1920x1080:rate=60:duration=10"))
-    // Generate test audio:
+    // Generate test audio
     .format("lavfi")
     .input("sine=frequency=1000:duration=10")
-    // Split video into one pipe output:
+    // Generate test subtitles
+    .format("srt")
+    .input(
+      "data:text/plain;base64,MQ0KMDA6MDA6MDAsMDAwIC0tPiAwMDowMDoxMCw1MDANCkhlbGxvIFdvcmxkIQ==",
+    )
+    // Video output
     .map("0:v")
     .format("rawvideo")
     .pix_fmt("rgb24")
-    .args(["-flush_packets", "1"])
     .output(VIDEO_PIPE_NAME)
-    // Split audio onto the other pipe output:
+    // Audio output
     .map("1:a")
     .format("s16le")
-    .args(["-flush_packets", "1"])
-    .output(AUDIO_PIPE_NAME);
+    .output(AUDIO_PIPE_NAME)
+    // Subtitles output
+    .map("2:s")
+    .format("srt")
+    .output(SUBTITLES_PIPE_NAME);
 
   // Create a separate thread for each output pipe
-  let threads = [VIDEO_PIPE_NAME, AUDIO_PIPE_NAME]
+  let threads = [VIDEO_PIPE_NAME, AUDIO_PIPE_NAME, SUBTITLES_PIPE_NAME]
     .iter()
     .map(|pipe_name| {
       let (ready_sender, ready_receiver) = mpsc::channel::<()>();
@@ -71,10 +79,12 @@ fn main() -> Result<()> {
         }
 
         // Exit
-        println!(
-          "[{pipe_name}] done reading ({}KiB total)",
-          total_bytes_read / 1024
-        );
+        let size_str = if total_bytes_read < 1024 {
+          format!("{}B", total_bytes_read)
+        } else {
+          format!("{}KiB", total_bytes_read / 1024)
+        };
+        println!("[{pipe_name}] done reading ({size_str} total)");
         Ok(())
       });
 
