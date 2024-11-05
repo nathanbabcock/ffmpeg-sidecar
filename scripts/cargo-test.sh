c0b95dc8f01bbb11a0d07edc78ce8324191c7e7f
nbabcock <nathan.r.babcock@gmail.com>
2024-11-04 18:12:09 -0600

    refactor: clean up scripts

--- scripts/cargo-test.sh
+++ scripts/cargo-test.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
+
 # Backtrace is on by default, and does not respect Cargo.toml
 # [env] section, even with force = true.
-export RUST_BACKTRACE=0
-cargo test
+RUST_BACKTRACE=0 cargo test
